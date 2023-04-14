c02db05de1158a1ce2cdc9e293a555d48be6c234
Vlad <hrom012k@gmail.com>
2023-04-14 19:34:17 +0300

    add eslint

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 require('dotenv').config({path: `${__dirname}/../.env`});
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT;
-app.get('/',(req, res)=>{
-   res.send('Girl GPT')
+app.get('/',(req, res) => {
+  res.send('Girl GPT');
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening on port ${PORT}...`)
-})
+app.listen(PORT,() => {
+  console.log(`Server is listening on port ${PORT}...`);
+});
