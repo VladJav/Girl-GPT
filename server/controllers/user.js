b0195f013d440f22b06b5c4ed92c13cb4be5fa97
Vlad <hrom012k@gmail.com>
2023-04-19 20:28:55 +0300

    add get all users controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,8 +5,23 @@ const showCurrentUser = (req, res) => {
 };
 
 const getAllUsers = async (req, res) => {
-    const users = await User.find({});
-    res.json({ users });
+    const { page = 1, limit = 20, sort = '', isActivated, name } = req.query;
+
+    const queryObject = {};
+    if (isActivated) {
+        queryObject.isActivated = isActivated === 'true';
+    }
+    if (name) {
+        queryObject.name = name;
+    }
+    const sortQuery = sort.split(',').join(' ');
+    const skip = limit * (page - 1);
+
+    const users = await User.find(queryObject).limit(limit).skip(skip).sort(sortQuery);
+
+    const totalPages = Math.ceil(await User.countDocuments(queryObject) / limit);
+
+    res.json({ users, count: users.length, totalPages, currentPage: +page });
 };
 
 const getSingleUser = (req, res) => {
