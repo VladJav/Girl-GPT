e8998c001529c885303b80c6f4343b809268cbef
Vlad <hrom012k@gmail.com>
2023-04-20 18:16:33 +0300

    feat: update error handler middleware for CastError

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
+const { StatusCodes } = require('http-status-codes');
 const errorHandler = (err, req, res, next) => {
-    res.status(err.statusCode || 500).json({ msg: err.message });
+
+    if (err instanceof mongoose.Error.CastError) {
+        res.status(StatusCodes.BAD_REQUEST).json({ msg: `No item found with id: ${err.value}` });
+        return;
+    }
+    res.status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR).json({
+        msg: err.message || 'Something went wrong try again later',
+    });
 };
 
 module.exports = errorHandler;
