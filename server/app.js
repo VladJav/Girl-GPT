e017281049073e09a48930e151ccada7c0e9a764
Vlad <hrom012k@gmail.com>
2023-04-22 21:44:00 +0300

    feat: add HTTP request logger middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
 const connectDb = require('./db/connect');
 
@@ -16,6 +17,7 @@ const notFoundMiddleware = require('./middleware/not-found');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(morgan('tiny'));
 app.use(cookieParser(process.env.JWT_REFRESH_SECRET));
 app.use(express.json());
 
