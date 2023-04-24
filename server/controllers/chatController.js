7a52fec34fc2973df8403898021664ca5431672a
Vlad <hrom012k@gmail.com>
2023-04-24 20:23:50 +0300

    feat: add get all/single chat controller, create chat

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,13 +1,35 @@
-const getAllChats = (req, res) => {
-    res.send('Get All Chats');
+const Chat = require('../models/Chat');
+const { NotFountError } = require('../errors');
+const { checkPermissions } = require('../utils');
+
+const getAllChats = async (req, res) => {
+    const { userId } = req.user;
+    
+    const chats = await Chat.find({ user: userId });
+
+    res.json({ chats });
 };
 
-const getSingleChat = (req, res) => {
-    res.send('Get Single Chat');
+const getSingleChat = async (req, res) => {
+    const { userId } = req.user;
+    const { id: chatId } = req.params;
+
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+        throw new NotFountError('Chat not found');
+    }
+
+    checkPermissions(req.user, chat.user);
+
+    res.json({ chat });
 };
 
-const createChat = (req, res) => {
-    res.send('Create Chat');
+const createChat = async (req, res) => {
+    const { userId } = req.user;
+    
+    const chat = await Chat.create({ user: userId });
+    
+    res.json({ chat });
 };
 const deleteChat = (req, res) => {
     res.send('Delete Chat');
