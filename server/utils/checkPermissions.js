d8c8082a7a1f2b1b2bec00818fe648850462d2c0
Vlad <hrom012k@gmail.com>
2023-04-24 20:23:03 +0300

    feat: update checkPermissions util function

--- server/utils/checkPermissions.js
+++ server/utils/checkPermissions.js
@@ -3,7 +3,7 @@ const checkPermissions = (requestUser, resourceUserId) => {
     if (requestUser.role === 'admin') {
         return;
     }
-    if (requestUser.userId === resourceUserId) {
+    if (requestUser.userId === resourceUserId.toString()) {
         return;
     }
     throw new PermissionError('You do not have enough permission');
