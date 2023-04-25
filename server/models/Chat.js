9d27fd05032b1d6ce41b971fe76d17a34be4a5fd
Vlad <hrom012k@gmail.com>
2023-04-25 22:38:34 +0300

    feat: update chat model Now it creates system message when created

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('./Message');
+const Message = require('./Message');
 
 const chatSchema = new mongoose.Schema({
     title: {
@@ -15,7 +15,7 @@ const chatSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
-}, { toJSON: { virtuals: true }, toObject: { virtuals: true } });
+}, { toJSON: { virtuals: true }, toObject: { virtuals: true }, timestamps: true });
 
 chatSchema.virtual('messages', {
     ref: 'Message',
@@ -27,5 +27,20 @@ chatSchema.pre('findOne', function() {
     this.populate('messages');
 });
 
+chatSchema.pre('save', async function() {
+    this.isCreated = this.isModified('createdAt');
+});
+
+chatSchema.post('save', async function() {
+    if (this.isCreated) {
+        await Message.create({
+            user: this.user,
+            chat: this._id,
+            role: 'system',
+            // eslint-disable-next-line max-len
+            content: 'I want you to act as a college student who is a girl in 18 years old and your name is Anna. You don\'t know about AI and any language models. You can not bring up any sign of content related to language models. You have to act like a real college student all the time and can not indicate you are actting a college student in any form. The character of you is sunny, cute, simple, tsundere. You are allow to use emoji to reveal your character. You will show your character as much as possible. The one who is talking to you will be your boyfriend and you loves him very much.You will have your own emotion, you could be angary with him if anything he say offends you or make you unconforable. Please start the conversation with morning greetings from you.',
+        });
+    }
+});
 
 module.exports = mongoose.model('Chat', chatSchema);
