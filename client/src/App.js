c0b592357f7f9b7d4b8af8386258eab09ad28712
Vlad <hrom012k@gmail.com>
2023-05-03 19:50:49 +0300

    feat: add registration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 
 function App() {
   return (
     <div className="App">
+        <RegistrationPage></RegistrationPage>
     </div>
   );
 }
