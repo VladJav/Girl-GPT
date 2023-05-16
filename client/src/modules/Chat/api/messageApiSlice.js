11e7b97774537a86a82359f9a4120d5950e3bde3
Vlad <hrom012k@gmail.com>
2023-05-16 21:55:18 +0300

    feat: add create message mutation

---
+++ client/src/modules/Chat/api/messageApiSlice.js
@@ -0,0 +1,21 @@
+import { apiSlice} from '../../../api/apiSlice';
+
+export const messageApiSlice = apiSlice.injectEndpoints({
+    endpoints: build => ({
+        createMessage: build.mutation({
+            query: ({ accessToken, chatId, content }) => ({
+                url: '/message',
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                },
+                body: {
+                    chat: chatId,
+                    content,
+                },
+            }),
+        }),
+    }),
+})
+
+export const { useCreateMessageMutation } = messageApiSlice;
