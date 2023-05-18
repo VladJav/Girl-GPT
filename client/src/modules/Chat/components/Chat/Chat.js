6704d9f9efdd6900a689e68caff304e9201b7742
Vlad <hrom012k@gmail.com>
2023-05-18 18:09:05 +0300

    feat: add text if no chat selected

--- client/src/modules/Chat/components/Chat/Chat.js
+++ client/src/modules/Chat/components/Chat/Chat.js
@@ -1,12 +1,12 @@
 import Grid from '@mui/material/Grid';
 import { useGetSingleChatQuery } from '../../api/chatApiSlice';
-import { TextField } from '@mui/material';
+import { TextField, Typography } from '@mui/material';
 import MessagesList from '../MessagesList/MessagesList';
 import { useCreateMessageMutation } from '../../api/messageApiSlice';
 
 export default function Chat(){
     const accessToken = localStorage.getItem('accessToken');
-    const chatId = '64651edf84fd13bc0a87e27a';
+    const chatId = '2';
 
     const { data, isSuccess } = useGetSingleChatQuery({
         accessToken,
@@ -18,13 +18,18 @@ export default function Chat(){
     const onSubmit = async (e) => {
         if(e.key === 'Enter'){
             const content = e.target.value;
-            const res = await createMessage({accessToken, chatId, content });
+            await createMessage({accessToken, chatId, content });
             e.target.value = '';
         }
-    }
+    };
+
     return (
         <Grid container justifyContent={'center'} height={'100vh'}>
-            {isSuccess && <MessagesList messages={data.chat.messages}/>}
+            {isSuccess ? <MessagesList messages={data.chat.messages}/> : <Grid container justifyContent={'center'} alignItems={'center'} >
+                <Typography  sx={{ fontWeight: 'bold' }} variant={'h3'}>
+                    Girl GPT
+                </Typography>
+            </Grid>}
             <Grid item xs={12} md={6}>
                 <TextField onKeyDown={onSubmit} variant="filled" sx={{ width: '50%' , backgroundColor: '#dedede', borderRadius: '5px', position: 'fixed', bottom: 0}} placeholder={'Send a message.'}/>
             </Grid>
