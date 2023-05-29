b8915bc0ee51171a9f237d45bb84d070932f19c1
Vlad <hrom012k@gmail.com>
2023-05-29 21:02:14 +0300

    feat: add select chat button

--- client/src/modules/ChatList/components/ChatList/ChatList.js
+++ client/src/modules/ChatList/components/ChatList/ChatList.js
@@ -1,21 +1,22 @@
-import { Grid, Typography } from '@mui/material';
-import { Button } from '@mui/material';
+import { Grid } from '@mui/material';
 import { useGetAllChatsQuery } from '../../api/getAllChatsApiSlice';
+import { useState } from 'react';
+import SelectChatButton from '../SelectChatButton/SelectChatButton';
 
 export default function ChatList(){
     const accessToken = localStorage.getItem('accessToken');
 
     const { data, isSuccess } = useGetAllChatsQuery({ accessToken });
+    const [clickedId, setClickedId] = useState(0);
 
+    const onClick = (clickedId) => {
+        setClickedId(clickedId);
+    }
     return (
-        <Grid container direction={'column'} xs={2} sx={{backgroundColor: '#f0f0f0', height: '100vh'}}>
-            <Grid container overflow={'auto'}>
+        <Grid container direction={'column'} md={2} xs={5} sx={{backgroundColor: '#f0f0f0', height: '100vh'}}>
+            <Grid container sx={{overflowX: 'hidden', overflowY: 'scroll'}}>
                 {isSuccess && data.chats.map(e=>{
-                        return <Grid xs={12} item key={e._id}>
-                            <Button sx={{width: '100%', color: '#414141'}} variant="text">
-                                <Typography>{e.title}</Typography>
-                            </Button>
-                        </Grid>
+                    return <SelectChatButton chat={e} onClick={onClick} selectedChatId={clickedId}/>
                     })}
             </Grid>
         </Grid>
---
+++ client/src/modules/ChatList/components/SelectChatButton/SelectChatButton.js
@@ -0,0 +1,36 @@
+import { Button, Grid, IconButton, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+const buttonStyles = {
+    width: '100%',
+    color: '#414141',
+}
+export default function SelectChatButton({ chat, onClick, selectedChatId }){
+
+    return (
+        <Grid xs={12} item>
+            <Button onClick={()=> {onClick(chat._id)}} sx={buttonStyles} variant="text">
+                <Grid container xs={12}>
+                    <Grid item xs={9} md={8}>
+                        <Typography sx={{ overflow: 'hidden', textAlign: 'left'}}>{chat.title}</Typography>
+                    </Grid>
+                    <Grid container xs={3} md={4}>
+                        <Grid item xs={6}>
+                            <IconButton size="small" onMouseDown={(e)=>{
+                                e.stopPropagation();}}>
+                                {selectedChatId===chat._id && <EditIcon/>}
+                            </IconButton>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <IconButton size="small" onMouseDown={(e)=>{
+                                e.stopPropagation();}}>
+                                {selectedChatId===chat._id && <DeleteIcon/>}
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Button>
+        </Grid>
+    )
+}
