35b5333da0d96d65a6862ea0c8f3177a021a5086
Vlad <hrom012k@gmail.com>
2023-06-03 13:34:40 +0300

    fix: delete useless state in chat list

--- client/src/modules/ChatList/components/ChatList/ChatList.js
+++ client/src/modules/ChatList/components/ChatList/ChatList.js
@@ -1,19 +1,17 @@
 import { Grid } from '@mui/material';
 import { useGetAllChatsQuery } from '../../api/getAllChatsApiSlice';
-import { useState } from 'react';
 import SelectChatButton from '../SelectChatButton/SelectChatButton';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { changeChat } from '../../slices/currentChatSlice';
 
 export default function ChatList(){
     const accessToken = localStorage.getItem('accessToken');
 
     const dispatch = useDispatch();
+    const { id: currentChat } = useSelector(state => state.currentChat);
     const { data, isSuccess } = useGetAllChatsQuery({ accessToken });
-    const [clickedId, setClickedId] = useState(0);
 
     const onClick = (clickedId) => {
-        setClickedId(clickedId);
         dispatch(
             changeChat({
                 id: clickedId,
@@ -24,7 +22,7 @@ export default function ChatList(){
         <Grid container direction={'column'} sx={{backgroundColor: '#f0f0f0', height: '100vh'}}>
             <Grid container sx={{overflowX: 'hidden', overflowY: 'scroll'}}>
                 {isSuccess && data.chats.map(e=>{
-                    return <SelectChatButton chat={e} onClick={onClick} selectedChatId={clickedId}/>
+                    return <SelectChatButton chat={e} onClick={onClick} selectedChatId={currentChat}/>
                     })}
             </Grid>
         </Grid>
