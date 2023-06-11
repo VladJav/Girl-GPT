4aab4f736b479d42adb55a8b9bbc9f7750c1c644
Vlad <hrom012k@gmail.com>
2023-06-11 20:37:50 +0300

    feat: add cors

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const connectDb = require('./db/connect');
 
@@ -19,7 +20,9 @@ const notFoundMiddleware = require('./middleware/not-found');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+
 app.use(morgan('tiny'));
+app.use(cors());
 app.use(cookieParser(process.env.JWT_REFRESH_SECRET));
 app.use(express.json());
 app.use(express.static('public'));
